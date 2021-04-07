c4d13e535d78c5e577043aeacfcffbf705bb4e50
stan000 <stanmpakatig@gmail.com>
2021-04-07 21:15:51 +0200

    added services

---
+++ lib/pages/add_text_widget.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter/material.dart';
+
+// Widget textInputBox() {
+//   return Row(
+//     children: [
+//       Flexible(
+//                    child: new TextField(
+//                      controller: _textController,
+//                      onChanged: (String messageText) {
+
+//                      },
+//                      onSubmitted: _sendText,
+//                      decoration:
+//                          new InputDecoration.collapsed(hintText: "Send a message"),
+//                    ),
+//                  ),
+//                  new Container(
+//                    margin: const EdgeInsets.symmetric(horizontal: 4.0),
+//                    child: IconButton(
+//          icon: new Icon(Icons.send),
+//          onPressed: _isWritting
+//              ? () => _sendText(_textController.text)
+//              : null,
+//        );  ,
+//                  ),
+//     ],
+//   );
+// }
--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ryan_app/models/message.dart';
+import 'package:ryan_app/pages/chatbuble.dart';
+import 'package:ryan_app/services/api.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -6,12 +9,55 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  TextEditingController _textController = TextEditingController();
+  bool _isWritting = false;
+
+  void _sendText(String text) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Chat'),
       ),
+      body: FutureBuilder(
+          future: TalkMeService().getMessages(),
+          builder: (context, snapshot) {
+            return ListView(
+              children: [
+                chatBubble(
+                  Message(
+                    message: 'Hey, Idiot, wassup',
+                    timeSent: DateTime.now(),
+                    userId: '892',
+                  ),
+                ),
+                // Text editing box
+                Row(
+                  children: [
+                    Flexible(
+                      child: new TextField(
+                        controller: _textController,
+                        onChanged: (String messageText) {},
+                        onSubmitted: _sendText,
+                        decoration: new InputDecoration.collapsed(
+                            hintText: "Send a message"),
+                      ),
+                    ),
+                    new Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: IconButton(
+                        icon: new Icon(Icons.send),
+                        onPressed: _isWritting
+                            ? () => _sendText(_textController.text)
+                            : null,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            );
+          }),
     );
   }
 }
